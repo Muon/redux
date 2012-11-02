1204be6b0b5eb040739326ecfc1dc9b5ff8f04fd
Mak Nazečić-Andrlon <owlberteinstein@gmail.com>
2012-11-02 22:20:44 +0100

    Add some tests for codegening arithmetic ops and relational ops

--- redux/test/codegen_test.py
+++ redux/test/codegen_test.py
@@ -3,13 +3,24 @@ from redux.codegenerator import compile_script
 
 def test_code_generation():
     code_examples = [
-        ("a = 1 + 1", "int a = (1+1);"),
         ("if 2 > 1 else end", "if((2>1)){\n}\nelse {\n}"),
         ("while 2 > 1 end", "while(1){\nif((2>1)){\n}\nelse {\nbreak;\n}\n}"),
         ("def f(a) return a end x = f(1)", "int __retval0 = 0;\n{\nint a = 1;\n__retval0 = a;\n}\nint x = __retval0;"),
         ("say(1, 2)", "say 1, 2;"),
         ("sqrt(2)", "(|/2);"),
-        ("a = \"foo\" say(a)", "say \"foo\";")
+        ("a = \"foo\" say(a)", "say \"foo\";"),
+        ("a = 1 - 1", "int a = (1-1);"),
+        ("a = 1 + 1", "int a = (1+1);"),
+        ("a = 1 * 1", "int a = (1*1);"),
+        ("a = 1 / 1", "int a = (1/1);"),
+        ("a = 1 > 1", "int a = (1>1);"),
+        ("a = 1 < 1", "int a = (1<1);"),
+        ("a = 1 <= 1", "int a = (1<=1);"),
+        ("a = 1 >= 1", "int a = (1>=1);"),
+        ("a = 1 == 1", "int a = (1==1);"),
+        ("a = 1 != 1", "int a = (1!=1);"),
+        ("a = 1 and 1", "int a = (1&&1);"),
+        ("a = 1 or 1", "int a = (1||1);"),
     ]
 
     for redux_code, rescript_code in code_examples:
