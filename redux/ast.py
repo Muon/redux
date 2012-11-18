3da87c932a2825487be113386e1246218460e196
Mak Nazečić-Andrlon <owlberteinstein@gmail.com>
2012-11-18 15:31:18 +0100

    Rewrite AST nodes

--- redux/ast.py
+++ redux/ast.py
@@ -1,72 +1,98 @@
-from redux.autorepr import AutoRepr
-from redux.structeq import StructEq
+class ASTNode(object):
+    """Base class of all AST nodes."""
+    _fields = []
+
+    def __init__(self, *args):
+        assert len(args) == len(self._fields), "field/argument length mismatch"
+
+        super(ASTNode, self).__init__()
+
+        for name, value in zip(self._fields, args):
+            setattr(self, name, value)
+
+    # Inspired by the standard ast module
+    def fields(self):
+        for field in self._fields:
+            try:
+                yield getattr(self, field)
+            except AttributeError:
+                pass
+
+    def children(self):
+        for name, value in self.fields():
+            if isinstance(value, ASTNode):
+                yield value
+            elif isinstance(value, list):
+                for item in value:
+                    if isinstance(item, ASTNode):
+                        yield item
+
+    def __eq__(self, other):
+        if type(other) is type(self):
+            return self.__dict__ == other.__dict__
+        else:
+            return False
 
 
-class ASTNode(AutoRepr, StructEq):
+class Stmt(ASTNode):
     pass
 
 
-class Block(ASTNode):
-    def __init__(self, statements):
-        self.statements = statements
-
-
-class FunctionCall(ASTNode):
-    def __init__(self, function, arguments):
-        self.function = function
-        self.arguments = arguments
-
-
-class ReturnStmt(ASTNode):
-    def __init__(self, expression):
-        self.expression = expression
-
-
-class BreakStmt(ASTNode):
+class Expr(ASTNode):
     pass
 
 
-class CodeLiteral(ASTNode):
-    def __init__(self, code):
-        self.code = code
+class Block(Stmt):
+    _fields = ["statements"]
 
 
-class Assignment(ASTNode):
+class FunctionCall(Expr, Stmt):
+    _fields = ["function", "arguments"]
+
+
+class ReturnStmt(Stmt):
+    _fields = ["expression"]
+
+
+class BreakStmt(Stmt):
+    pass
+
+
+class CodeLiteral(Stmt):
+    _fields = ["code"]
+
+
+class Assignment(Stmt):
+    _fields = ["variable", "expression", "shadow"]
+
     def __init__(self, variable, expression, shadow=False):
-        self.variable = variable
-        self.expression = expression
-        self.shadow = shadow
+        super(Assignment, self).__init__(variable, expression, shadow)
 
 
 class BitfieldAssignment(Assignment):
     pass
 
 
-class IfStmt(ASTNode):
+class IfStmt(Stmt):
+    _fields = ["condition", "then_block", "else_part"]
+
     def __init__(self, condition, then_block, else_part=None):
-        self.condition = condition
-        self.then_block = then_block
-        self.else_part = else_part
+        super(IfStmt, self).__init__(condition, then_block, else_part)
 
 
-class WhileStmt(ASTNode):
-    def __init__(self, condition, block):
-        self.condition = condition
-        self.block = block
+class WhileStmt(Stmt):
+    _fields = ["condition", "block"]
 
 
-class FunctionDefinition(ASTNode):
+class FunctionDefinition(Stmt):
+    _fields = ["name", "arguments", "block", "nontrivial"]
+
     def __init__(self, name, arguments, block):
-        self.name = name
-        self.arguments = arguments
-        self.block = block
-        self.nontrivial = True
+        super(Stmt, self).__init__(name, arguments, block, True)
 
 
-class BitfieldDefinition(ASTNode):
-    def __init__(self, name, members):
-        self.name = name
-        self.members = members
+class BitfieldDefinition(Stmt):
+    _fields = ["name", "members"]
 
     def get_member_limits(self, member):
         offset = 0
@@ -79,45 +105,46 @@ class BitfieldDefinition(ASTNode):
         raise KeyError(member)
 
 
-class EnumDefinition(ASTNode):
+class EnumDefinition(Stmt):
+    _fields = ["name", "members"]
+
     def __init__(self, name, members):
-        self.name = name
         computed_members = []
 
         counter = -1
-        for name, value in members:
+        for member_name, value in members:
             if value is None:
                 counter += 1
             else:
                 assert value > counter
                 counter = value
 
-            computed_members.append((name, counter))
+            computed_members.append((member_name, counter))
 
-        self.members = computed_members
+        super(Stmt, self).__init__(name, computed_members)
 
 
-class Constant(ASTNode):
+class Constant(Expr):
+    _fields = ["value", "type"]
+
     def __init__(self, value, type_):
         self.value = value
         self.type = type_
 
 
-class VarRef(ASTNode):
+class VarRef(Expr):
+    _fields = ["name"]
+
     def __init__(self, name):
         self.name = name
 
 
-class BitfieldAccess(ASTNode):
-    def __init__(self, variable, member):
-        self.variable = variable
-        self.member = member
+class BitfieldAccess(Expr):
+    _fields = ["variable", "member"]
 
 
-class BinaryOp(ASTNode):
-    def __init__(self, lhs, rhs):
-        self.lhs = lhs
-        self.rhs = rhs
+class BinaryOp(Expr):
+    _fields = ["lhs", "rhs"]
 
 
 class AddOp(BinaryOp):
@@ -172,6 +199,9 @@ class LogicalOrOp(RelationalOp, BinaryOp):
     pass
 
 
-class LogicalNotOp(RelationalOp):
-    def __init__(self, expression):
-        self.expression = expression
+class UnaryOp(Expr):
+    _fields = ["expression"]
+
+
+class LogicalNotOp(RelationalOp, UnaryOp):
+    pass
